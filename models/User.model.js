f053ab21261635593f26770cd5cd4f68628acda5
anac-jeronimo <anac_jeronimo@hotmail.com>
2020-11-11 17:16:57 +0000

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,26 @@
 // User model here
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String, 
+            trim: true,
+            required: [true, 'Username is required'],
+            unique: true
+        }, 
+        email: {
+            type: String,
+            trim: true,
+            required: [true, 'Email is required'],
+            unique: true
+        }, 
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        }
+}
+)
+
+module.exports = model('User', userSchema);
